2dedec7a040516dd4d29b9ea81bc3226d6278913
Vural Bayrakli <vuralll360@gmail.com>
2023-02-01 22:12:41 +0300

    initial commit

---
+++ hello.py
@@ -0,0 +1,41 @@
+from flask import Flask, render_template
+
+app = Flask(__name__)
+
+@app.route("/")
+def index():
+    stuff = "This is <strong>Bold</strong> text"
+    favos = ["apple", "banana", "orange"]
+    return render_template("index.html", stuff = stuff, favos = favos)
+
+@app.route("/user/<name>")
+def user(name):
+    return render_template("user.html", name =  name)
+
+#Invalid Error
+@app.errorhandler(404)
+def page_not_found(e):
+    return render_template("404.html"), 404
+
+# Internal server error
+@app.errorhandler(500)
+def page_not_found(e):
+    return render_template("500.html"), 500
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
